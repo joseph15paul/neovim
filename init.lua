eb8980202f499d228ce7b8b0dc7c74c52c527df7
Joseph Paul <joseph15paul@gmail.com>
2026-02-26 01:06:23 +0530

    More plugins

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
 require("vim-options")
 require("config.lazy")
 
-vim.keymap.set('n', '<leader>fs', ':Neotree filesystem reveal left <CR>', {})
-
-require'nvim-treesitter'.setup {
-  -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
-  install_dir = vim.fn.stdpath('data') .. '/site'
-}
-
-require'nvim-treesitter'.install { 'lua','python', 'c', 'cpp', 'c_sharp', 'docker_file', 'rust', 'javascript', 'zig' }
---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,5 @@
+return {
+  "tpope/vim-fugitive",
+  cmd = { "Git", "G" }, -- Lazy-load only on :Git
+}
+
